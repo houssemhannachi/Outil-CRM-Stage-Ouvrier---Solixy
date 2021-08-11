20ee081d147cdc281297225d3cd89adb6c5588ab
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-11 11:21:09 +0100

    Create prospects.php

---
+++ prospects.php
@@ -0,0 +1,23 @@
+<link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+		
+		<!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css?v=<?php echo time();?>">
+		
+		<!-- Fontawesome CSS -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css?v=<?php echo time();?>">
+    
+		<!-- Lineawesome CSS -->
+        <link rel="stylesheet" href="assets/css/line-awesome.min.css?v=<?php echo time();?>">
+		
+		<!-- Main CSS -->
+        <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time();?>">
+<?php
+$pageName = "Devis";
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {}
+
+?>
+<?php require "dashboard.php";?>
+
+<script src="script.js?v=<?php echo time();?>"></script>
