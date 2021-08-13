3046491e0653d07035f1214d9f38122e83286467
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-13 15:43:41 +0100

    Update header.php

--- header.php
+++ header.php
@@ -5,6 +5,11 @@
     <title> <?php echo $pageName ?> </title>
     <link rel="stylesheet" href="css/styledash.css?v=<?php echo time();?>" >
     <link rel="stylesheet" href="css/style.css?v=<?php echo time();?>" >
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css?v=<?php echo time();?>">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="assets/css/line-awesome.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="assets/css/select2.min.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time();?>">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
