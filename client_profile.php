5ea09769f5c5fce8e25cc765e694010cc3c21d78
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-14 10:42:35 +0100

    Update client_profile.php

--- client_profile.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once ("db_conn.php");
-$pageName = "Clients";
-session_start();
-
-if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {}
-
-?>
-<?php require "dashboard.php";?>
