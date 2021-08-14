d28ce5275ca9936706824b73866540bdd5f293b2
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-14 11:47:07 +0100

    Update ajouter_client.php

--- ajouter_client.php
+++ ajouter_client.php
@@ -2,22 +2,26 @@
 require_once ("db_conn.php");
 
 if (isset($_POST['submit'])){
-    if (empty($_POST['nom'])|| empty($_POST['email'])|| empty($_POST['reference'])|| empty($_POST['adresse'])|| empty($_POST['pays'])|| empty($_POST['tel'])|| empty($_POST['matricule']) )
+    if (empty($_POST['raisonsociale'])|| empty($_POST['reference'])|| empty($_POST['formejuridique'])|| empty( $_POST['email'])|| empty($_POST['adresse'])|| empty($_POST['ville'])|| empty($_POST['pays']) )
     {
         echo "Please fill the blanks";
     }
     else {
-        $nom = $_POST['nom'];
+        $raisonsociale = $_POST['raisonsociale'];
         $reference = $_POST['reference'];
-        $adresse = $_POST['adresse'];
+        $formejuridique = $_POST['formejuridique'];
         $email = $_POST['email'];
+        $adresse = $_POST['adresse'];
         $ville = $_POST['ville'];
         $pays = $_POST['pays'];
-        $tel = $_POST['tel'];
-        $matricule = $_POST['matricule'];
+        $telephone = $_POST['telephone'];
+        $siteweb = $_POST['siteweb'];
+        $matriculefiscale = $_POST['matriculefiscale'];
+        $ribrip = $_POST['ribrip'];
+        $tauxtva = $_POST['tauxtva'];
         
 
-        $query = "Insert into clients (nom_client,reference_client,adresse_client,email_client,ville_client,pays_client,tel_client,matricule_client) values ('$nom','$reference','$adresse','$email','$ville','$pays','$tel','$matricule')";
+        $query = "INSERT INTO clients (rs_client,ref_client,fj_client,email_client,adresse_client,ville_client,pays_client,tel_client,sw_client,mf_client,riprib_client,tauxtva_client) VALUES ('$raisonsociale','$reference','$formejuridique','$email','$adresse','$ville','$pays','$telephone','$siteweb','$matriculefiscale','$ribrip','$tauxtva' )";
         $result = mysqli_query($conn,$query);
 
         if($result) {
