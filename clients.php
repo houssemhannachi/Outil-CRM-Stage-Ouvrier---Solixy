a0e61f4e206949d420a11af9ef5cc619a537aeb0
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-17 11:42:37 +0100

    Update clients.php

--- clients.php
+++ clients.php
@@ -72,9 +72,9 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 							<table class="table table-striped custom-table datatable">
 								<thead>
 									<tr>
-										<th class="none">ID</th>
-										<th>Raison sociale</th>
+										<th>ID</th>
 										<th>Référence</th>
+										<th>Raison sociale</th>
 										<th>Adresse</th>
 										<th>Email</th>
 										<th>Téléphone</th>
@@ -92,8 +92,8 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 								<?php 
 									while($row=mysqli_fetch_assoc($result)) {
 										$id_client = $row['id_client'];
-										$rs_client = $row['rs_client'];
 										$ref_client = $row['ref_client'];
+										$rs_client = $row['rs_client'];
 										$fj_client = $row['fj_client'];
      									$email_client = $row['email_client'];
         								$adresse_client = $row['adresse_client'];
@@ -106,9 +106,10 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 										$tauxtva_client = $row['tauxtva_client'];
 								?>
 									<tr>
-										<td class="none"><?php echo $id_client?></td>
-										<td><?php echo $rs_client?></td>
+										<td><?php echo $id_client?></td>
+										
 										<td><?php echo $ref_client?></td>
+										<td><?php echo $rs_client?></td>
 										<td><?php echo $adresse_client?></td>
 										<td><?php echo $email_client?></td>
 										<td><?php echo $tel_client?></td>
@@ -123,9 +124,10 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 											<div class="dropdown dropdown-action">
 												<a href="#" class="action-icon dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
 												<div class="dropdown-menu dropdown-menu-right">
+													<a class="dropdown-item"><i class="fa fa-id-card m-r-5"></i> Profile</a>
+													<a class="dropdown-item detailsbtn"><i class="fa fa-id-card m-r-5"></i> Détails</a>
 													<a class="dropdown-item editbtn"><i class="fa fa-pencil m-r-5"></i> Modifier</a>
 													<a class="dropdown-item deletebtn"><i class="fa fa-trash-o m-r-5"></i> Supprimer</a>
-													<a class="dropdown-item detailsbtn"><i class="fa fa-id-card m-r-5"></i> Détails</a>
 												</div>
 											</div>
 										</td>
