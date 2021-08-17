309db564e736f8693b2390236bc9fe995a9c8318
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-17 17:12:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -156,6 +156,19 @@ $(document).ready(function(){
   });
 });
 
+$(document).ready(function(){
+  $('.deletebtnpaiement').on('click', function(){
+    $('#effacer_paiement').modal('show');
+    $tr = $(this).closest('tr');
+    var data = $tr.children("td").map(function(){
+      return $(this).text();
+    }).get();
+    console.log(data);
+
+    $('#delete_id ').val(data[0]);
+
+  });
+});
 
 
 
