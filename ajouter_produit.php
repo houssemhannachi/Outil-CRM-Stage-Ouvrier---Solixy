2b004f883d24e841ddd0116aa9072e3caabba5be
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-19 13:12:47 +0100

    Create ajouter_produit.php

---
+++ ajouter_produit.php
@@ -0,0 +1,47 @@
+<?php
+require_once ("db_conn.php");
+
+if (isset($_POST['submit'])){
+    if (empty($_POST['reference'])|| empty($_POST['designation'])|| empty($_POST['quantite'])|| empty( $_POST['puht'])|| empty($_POST['montantht']) )
+    {
+        echo "Please fill the blanks";
+    }
+    else {
+        $reference = $_POST['reference'];
+        $designation = $_POST['designation'];
+        $quantite = $_POST['quantite'];
+        $puht = $_POST['puht'];
+        $montantht = $_POST['montantht'];
+       
+
+        $query = "INSERT INTO produits (reference,designation,quantite,puht,montantht) VALUES ('$reference','$designation','$quantite','$puht','$montantht')";
+        $result = mysqli_query($conn,$query);
+
+        if($result) {
+            header ("location:ajouter_facture.php");
+
+        }
+        else 
+        {
+            echo "Please check your query";
+
+        }
+
+
+    }
+
+}
+else
+{
+    header ('location:facture.php');
+
+}
+
+
+
+
+
+
+
+
+?>
