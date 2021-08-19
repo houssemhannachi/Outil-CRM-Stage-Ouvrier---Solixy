108ffdf8439a7ded54517683601b15cf686231a3
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-19 17:21:31 +0100

    Update ajouter_facture.php

--- ajouter_facture.php
+++ ajouter_facture.php
@@ -14,7 +14,7 @@
 
 <?php 
 	require_once('db_conn.php');
-	$query = "select * from produits";
+	$query = "select * from products";
 	$result = mysqli_query($conn,$query);
 ?>
 <?php
@@ -108,156 +108,125 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 			</div>
 
 
-					<div class="row">
-						<div class="col-sm-12">
-							<form  action = "ajouter_fact.php" method ="POST">
-								<div class="row">
-									<div class="col-sm-6 col-md-3">
-										<div class="form-group">
-											<label>ID Client <span class="text-danger">*</span></label>
-											<input class="form-control" name ="idclient" type="text">
-										</div>
-									</div>
-									<div class="col-sm-6 col-md-3">
-										<div class="form-group">
-											<label>Client <span class="text-danger">*</span></label>
-											<input class="form-control" name ="client" type="text">
-										</div>
-									</div>
-									
-									<div class="col-sm-6 col-md-3">
-                         <div class="form-group">
-											<label>Matricule fiscale <span class="text-danger">*</span> </label>
-											<input class="form-control" name="matriculefiscale" type="text">
-										</div>
-									</div>
-                                    <div class="col-sm-6 col-md-3">
-										<div class="form-group">
-											<label> Date <span class="text-danger">*</span></label>
-											
-												<input class="form-control" type="date" name="date">
-
-										</div>
-									</div>
-									
-
-								</div>
-								<div class="row">
-									<div class="col-md-12 col-sm-12">
-										<div class="table-responsive">
-											<table class="table table-hover table-white" id="dynamic_field">
-												<thead>
-													<tr>
-														<th style="width: 20px"></th>
-														<th class="col-sm-2">RÉF</th>
-														<th class="col-md-6">DÉSIGNATION</th>
-														<th style="width:100px;">QTÉ</th>
-														<th style="width:80px;">PU HT</th>
-														<th>MONTANT HT</th>
-														<th> </th>
-													</tr>
-												</thead>
-												<tbody>
-												<?php 
-									while($row=mysqli_fetch_assoc($result)) {
-										
-										$id_produit = $row['id_produit'];
-										$reference = $row['reference'];
-										$designation = $row['designation'];
-										$quantite = $row['quantite'];
-     									$puht = $row['puht'];
-        								$montantht = $row['montantht'];
-								?>
-									<tr>
-										<td><?php echo $id_produit?></td>
-										
-										<td><?php echo $reference?></td>
-										<td><?php echo $designation?></td>
-										<td><?php echo $quantite?></td>
-										<td><?php echo $puht?></td>
-										<td><?php echo $montantht?></td>
-									</tr>
-									<?php }
-									?>
+			<div class="row">
+			<div class="col-md-10 mx-auto">
+				<div class="card" style="box-shadow:0 0 25px 0 lightgrey;">
+				  <div class="card-header">
+				   	<h4>New Orders</h4>
+				  </div>
+				  <div class="card-body">
+				  	<form id="get_order_data" onsubmit="return false">
+				  		<div class="form-group row">
+				  			<label class="col-sm-3 col-form-label" align="right">Order Date</label>
+				  			<div class="col-sm-6">
+				  				<input type="date" id="order_date" name="order_date" readonly class="form-control form-control-sm" >
+				  			</div>
+				  		</div>
+				  		<div class="form-group row">
+				  			<label class="col-sm-3 col-form-label" align="right">Customer Name*</label>
+				  			<div class="col-sm-6">
+				  				<input type="text" id="cust_name" name="cust_name"class="form-control form-control-sm" placeholder="Enter Customer Name" required/>
+				  			</div>
+				  		</div>
 
 
+				  		<div class="card" style="box-shadow:0 0 15px 0 lightgrey;">
+				  			<div class="card-body">
+				  				<h3>Make a order list</h3>
+				  				<table align="center" style="width:800px;">
+		                            <thead>
+		                              <tr>
+		                                <th>#</th>
+		                                <th style="text-align:center;">Item Name</th>
+		                                <th style="text-align:center;">Total Quantity</th>
+		                                <th style="text-align:center;">Quantity</th>
+		                                <th style="text-align:center;">Price</th>
+		                                <th>Total</th>
+		                              </tr>
+		                            </thead>
+		                            <tbody id="invoice_item">
+		<!--<tr>
+		    <td><b id="number">1</b></td>
+		    <td>
+		        <select name="pid[]" class="form-control form-control-sm" required>
+		            <option>Washing Machine</option>
+		        </select>
+		    </td>
+		    <td><input name="tqty[]" readonly type="text" class="form-control form-control-sm"></td>   
+		    <td><input name="qty[]" type="text" class="form-control form-control-sm" required></td>
+		    <td><input name="price[]" type="text" class="form-control form-control-sm" readonly></td>
+		    <td>Rs.1540</td>
+		</tr>-->
+		                            </tbody>
+		                        </table> <!--Table Ends-->
+		                        <center style="padding:10px;">
+		                        	<button id="add" style="width:150px;" class="btn btn-success">Add</button>
+		                        	<button id="remove" style="width:150px;" class="btn btn-danger">Remove</button>
+		                        </center>
+				  			</div> <!--Crad Body Ends-->
+				  		</div> <!-- Order List Crad Ends-->
 
-												<tr>
-													<td></td>
-													<td></td>
-													<td></td>
-													<td></td>
-													<td></td>
-													<td></td>
-													<td><a class="text-success font-18" name="Add" id="add"><i class="fa fa-plus"></i></a></td>
-												</tr>
+				  	<p></p>
+                    <div class="form-group row">
+                      <label for="sub_total" class="col-sm-3 col-form-label" align="right">Sub Total</label>
+                      <div class="col-sm-6">
+                        <input type="text" readonly name="sub_total" class="form-control form-control-sm" id="sub_total" required/>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="gst" class="col-sm-3 col-form-label" align="right">GST (18%)</label>
+                      <div class="col-sm-6">
+                        <input type="text" readonly name="gst" class="form-control form-control-sm" id="gst" required/>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="discount" class="col-sm-3 col-form-label" align="right">Discount</label>
+                      <div class="col-sm-6">
+                        <input type="text" name="discount" class="form-control form-control-sm" id="discount" required/>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="net_total" class="col-sm-3 col-form-label" align="right">Net Total</label>
+                      <div class="col-sm-6">
+                        <input type="text" readonly name="net_total" class="form-control form-control-sm" id="net_total" required/>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="paid" class="col-sm-3 col-form-label" align="right">Paid</label>
+                      <div class="col-sm-6">
+                        <input type="text" name="paid" class="form-control form-control-sm" id="paid" required>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="due" class="col-sm-3 col-form-label" align="right">Due</label>
+                      <div class="col-sm-6">
+                        <input type="text" readonly name="due" class="form-control form-control-sm" id="due" required/>
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="payment_type" class="col-sm-3 col-form-label" align="right">Payment Method</label>
+                      <div class="col-sm-6">
+                        <select name="payment_type" class="form-control form-control-sm" id="payment_type" required/>
+                          <option>Cash</option>
+                          <option>Card</option>
+                          <option>Draft</option>
+                          <option>Cheque</option>
+                        </select>
+                      </div>
+                    </div>
 
-												</tbody>
-											</table>
-										</div>
-										<div class="table-responsive">
-											<table class="table table-hover table-white">
-												<tbody>
-													<tr>
-														<td></td>
-														<td></td>
-														<td></td>
-														<td></td>
-														<td class="text-right">Total HT</td>
-														<td style="text-align: right; padding-right: 30px;width: 230px">0</td>
-													</tr>
-													<tr>
-														<td colspan="5" class="text-right">Taxe</td>
-														<td style="text-align: right; padding-right: 30px;width: 230px">
-															<input class="form-control" name ="taxe" type="number">
-														</td>
-													</tr>
-													<tr>
-														<td colspan="5" class="text-right">
-															TVA %
-														</td>
-														<td style="text-align: right; padding-right: 30px;width: 230px">
-															<input class="form-control text-right" name="tva"type="number">
-														</td>
+                    <center>
+                      <input type="submit" id="order_form" style="width:150px;" class="btn btn-info" value="Order">
+                      <input type="submit" id="print_invoice" style="width:150px;" class="btn btn-success d-none" value="Print Invoice">
+                    </center>
 
-														<tr>
-														<td colspan="5" class="text-right">
-															Timber fiscale 
-														</td>
-														<td style="text-align: right; padding-right: 30px;width: 230px">
-															<input class="form-control text-right" name="timberfiscale"type="number">
-														</td>
-													</tr>
-													<tr>
-														<td colspan="5" style="text-align: right; font-weight: bold">
-															Total Taxe
-														</td>
-														<td style="text-align: right; padding-right: 30px; font-weight: bold; font-size: 16px;width: 230px">
-														<input class="form-control text-right" name="totaletaxe"type="number">
 
-														</td>
-													</tr>
-													<tr>
-														<td colspan="5" style="text-align: right; font-weight: bold">
-															Total TTC
-														</td>
-														<td style="text-align: right; padding-right: 30px; font-weight: bold; font-size: 16px;width: 230px">
-														<input class="form-control" name ="montanttt" type="number">
-														</td>
-													</tr>
-												</tbody>
-											</table>                               
-										</div>
+				  	</form>
 
-									</div>
-								</div>
-								<div class="submit-section">
-									<button class="btn btn-primary submit-btn" name ="submit">Enregistrer</button>
-								</div>
-							</form>
-						</div>
-					</div>
+				  </div>
 				</div>
+			</div>
+		</div>				</div>
 				<!-- /Page Content -->
 				
 			</div>
