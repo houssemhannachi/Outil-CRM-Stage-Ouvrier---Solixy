5cde00eb6643982839a04439a52c08525af1b940
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-20 14:53:35 +0100

    Update header.php

--- header.php
+++ header.php
@@ -9,7 +9,7 @@
 		<link rel="stylesheet" href="assets/css/font-awesome.min.css?v=<?php echo time();?>">
     <link rel="stylesheet" href="assets/css/line-awesome.min.css?v=<?php echo time();?>">
     <link rel="stylesheet" href="assets/css/select2.min.css?v=<?php echo time();?>">
-    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time();?>">
+    <link rel="stylesheet" href="assets/css/styles.css?v=<?php echo time();?>">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
