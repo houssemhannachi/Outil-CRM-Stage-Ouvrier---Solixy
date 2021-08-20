e42a6cf49bb08d5674fc6d5a0d865ba6ca6305e1
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-21 00:41:44 +0100

    Update facture.php

--- facture.php
+++ facture.php
@@ -51,7 +51,7 @@ $invoice->checkLoggedIn();
 					<div class="col-sm-6 col-md-3">  
 						<div class="form-group form-focus">
 							<input type="text" class="form-control floating">
-							<label class="focus-label">Reference</label>
+							<label class="focus-label">Client</label>
 						</div>
 					</div>
 						
