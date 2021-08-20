82720978359e1705db31d1a9ec3eee79e633c984
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-20 14:34:12 +0100

    Update header.php

--- header.php
+++ header.php
@@ -11,6 +11,10 @@
     <link rel="stylesheet" href="assets/css/select2.min.css?v=<?php echo time();?>">
     <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time();?>">
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
    </head>
 <body>
