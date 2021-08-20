e3fa8e789f53572ed27d286fe7c0226421be2060
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-20 14:34:06 +0100

    Update ajouter_facture.php

--- ajouter_facture.php
+++ ajouter_facture.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="assets/css/styleinvoice.css?v=<?php echo time();?>">
 <link rel="stylesheet" href="assets/css/bootstrap.min.css?v=<?php echo time();?>">
 			
 			<!-- Fontawesome CSS -->
@@ -10,23 +11,32 @@
 			<link rel="stylesheet" href="assets/css/select2.min.css?v=<?php echo time();?>">
 			
 			<!-- Main CSS -->
-			<link rel="stylesheet" href="assets/css/style.css?v=<?php echo time();?>">
+			<link rel="stylesheet" href="assets/css/styles.css?v=<?php echo time();?>">
+         
+
 
 <?php 
-	require_once('db_conn.php');
-	$query = "select * from products";
-	$result = mysqli_query($conn,$query);
-?>
-<?php
-$pageName = "Facture";
-session_start();
+   session_start();
+   $_SESSION["name"]= "Solixy";
+   $_SESSION["address"]="Avenue de la république </br> Immeuble Al Ahram 4ème étage";
+   $_SESSION["ville"] = "Gabes - Tunisie";
+   $_SESSION["mobile"]="+216 75 270 938";
+  $pageName="Factures";
 
-if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+   include('dashboard.php');
+   include 'Invoice.php';
+   $pageName ="Ajouter facture";
+   $invoice = new Invoice();
+   
+   	
+   if(!empty($_POST['companyName']) && $_POST['companyName']) {	
+   	$invoice->saveInvoice($_POST);
+   	header("Location:invoice_list.php");
+      
+   }
+   ?>
 
-?>
-  <?php require "dashboard.php";?>
-  
-<!-- Main Wrapper -->
+<script src="js/invoice.js?v=<?php echo time();?>"></script>
 <div class="home-content">
 		<div class="page-wrapper">
 		
@@ -42,216 +52,161 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 								<li class="breadcrumb-item"><a href="home.php">Dashboard</a></li>
 								<li class="breadcrumb-item"><a href="facture.php">Facture</a></li>
                                 <li class="breadcrumb-item active">Ajouter une facture</li>
-								</ul>
-						</div>
-						<div class="col-auto float-right ml-auto">
-
-
-							<a href="#" class="btn add-btn" data-toggle="modal" data-target="#add_product"><i class="fa fa-plus"></i>Ajouter un produit</a>
-
+							</ul>
 						</div>
 					</div>
-					
-                </div>
-				<div id="add_product" class="modal custom-modal fade" role="dialog">
-				<div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title">Ajouter un Produit</h5>
-							<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-								<span aria-hidden="true">&times;</span>
-							</button>
-						</div>
-						<div class="modal-body">
-							<form action = "ajouter_produit.php" method ="POST">
-								<div class="row">
-									<div class="col-md-6">
-										<div class="form-group">
-											<label class="col-form-label">Référence<span class="text-danger">*</span></label>
-											<input class="form-control" name ="reference" type="text">
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="form-group">
-											<label class="col-form-label">Désignation<span class="text-danger">*</span></label>
-											<input class="form-control" name ="designation" type="text">
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="form-group">
-											<label class="col-form-label">Quantité <span class="text-danger">*</span></label>
-											<input class="form-control" name ="quantite" type="number">
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="form-group">
-											<label class="col-form-label">PU HT <span class="text-danger">*</span></label>
-											<input class="form-control floating" name ="puht" type="double">
-										</div>
-									</div>
-									<div class="col-md-6">
-										<div class="form-group">
-											<label class="col-form-label">Montant HT<span class="text-danger">*</span></label>
-											<input class="form-control" name ="montantht" type="double">
-										</div>
-									</div>
+            </div>
+               <div class="row">
+			        <div class="col-md-10 mx-auto">
+                  <div class="container content-invoice">
+                     <div class="cards">
+                       <div class="card-bodys">
+                          <form action="" id="invoice-form" method="post" class="invoice-form" role="form" novalidate="">
+      <div class="load-animate animated fadeInUp">
+         <div class="row">
+            <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8">
+              
+          
+            </div>
+         </div>
+         <input id="currency" type="hidden" value="$">
+         <div class="row">
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+               <h3>De,</h3>
+               <b><?php echo $_SESSION['name']; ?> </b><br> 
+               <?php echo $_SESSION['address']; ?><br>  <?php echo $_SESSION['ville']; ?><br> 
+               <?php echo $_SESSION['mobile']; ?><br>
 
-									
-								</div>
-								<div class="submit-section">
-									<button class="btn btn-primary submit-btn" name ="submit">Ajouter</button>
-								</div>
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-
-
-			<div class="row">
-			<div class="col-md-10 mx-auto">
-				<div class="card" style="box-shadow:0 0 25px 0 lightgrey;">
-				  <div class="card-header">
-				   	<h4>New Orders</h4>
-				  </div>
-				  <div class="card-body">
-				  	<form id="get_order_data" onsubmit="return false">
-				  		<div class="form-group row">
-				  			<label class="col-sm-3 col-form-label" align="right">Order Date</label>
-				  			<div class="col-sm-6">
-				  				<input type="date" id="order_date" name="order_date" readonly class="form-control form-control-sm" >
-				  			</div>
-				  		</div>
-				  		<div class="form-group row">
-				  			<label class="col-sm-3 col-form-label" align="right">Customer Name*</label>
-				  			<div class="col-sm-6">
-				  				<input type="text" id="cust_name" name="cust_name"class="form-control form-control-sm" placeholder="Enter Customer Name" required/>
-				  			</div>
-				  		</div>
-
-
-				  		<div class="card" style="box-shadow:0 0 15px 0 lightgrey;">
-				  			<div class="card-body">
-				  				<h3>Make a order list</h3>
-				  				<table align="center" style="width:800px;">
-		                            <thead>
-		                              <tr>
-		                                <th>#</th>
-		                                <th style="text-align:center;">Item Name</th>
-		                                <th style="text-align:center;">Total Quantity</th>
-		                                <th style="text-align:center;">Quantity</th>
-		                                <th style="text-align:center;">Price</th>
-		                                <th>Total</th>
-		                              </tr>
-		                            </thead>
-		                            <tbody id="invoice_item">
-		<!--<tr>
-		    <td><b id="number">1</b></td>
-		    <td>
-		        <select name="pid[]" class="form-control form-control-sm" required>
-		            <option>Washing Machine</option>
-		        </select>
-		    </td>
-		    <td><input name="tqty[]" readonly type="text" class="form-control form-control-sm"></td>   
-		    <td><input name="qty[]" type="text" class="form-control form-control-sm" required></td>
-		    <td><input name="price[]" type="text" class="form-control form-control-sm" readonly></td>
-		    <td>Rs.1540</td>
-		</tr>-->
-		                            </tbody>
-		                        </table> <!--Table Ends-->
-		                        <center style="padding:10px;">
-		                        	<button id="add" style="width:150px;" class="btn btn-success">Add</button>
-		                        	<button id="remove" style="width:150px;" class="btn btn-danger">Remove</button>
-		                        </center>
-				  			</div> <!--Crad Body Ends-->
-				  		</div> <!-- Order List Crad Ends-->
-
-				  	<p></p>
-                    <div class="form-group row">
-                      <label for="sub_total" class="col-sm-3 col-form-label" align="right">Sub Total</label>
-                      <div class="col-sm-6">
-                        <input type="text" readonly name="sub_total" class="form-control form-control-sm" id="sub_total" required/>
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="gst" class="col-sm-3 col-form-label" align="right">GST (18%)</label>
-                      <div class="col-sm-6">
-                        <input type="text" readonly name="gst" class="form-control form-control-sm" id="gst" required/>
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="discount" class="col-sm-3 col-form-label" align="right">Discount</label>
-                      <div class="col-sm-6">
-                        <input type="text" name="discount" class="form-control form-control-sm" id="discount" required/>
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="net_total" class="col-sm-3 col-form-label" align="right">Net Total</label>
-                      <div class="col-sm-6">
-                        <input type="text" readonly name="net_total" class="form-control form-control-sm" id="net_total" required/>
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="paid" class="col-sm-3 col-form-label" align="right">Paid</label>
-                      <div class="col-sm-6">
-                        <input type="text" name="paid" class="form-control form-control-sm" id="paid" required>
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="due" class="col-sm-3 col-form-label" align="right">Due</label>
-                      <div class="col-sm-6">
-                        <input type="text" readonly name="due" class="form-control form-control-sm" id="due" required/>
-                      </div>
-                    </div>
-                    <div class="form-group row">
-                      <label for="payment_type" class="col-sm-3 col-form-label" align="right">Payment Method</label>
-                      <div class="col-sm-6">
-                        <select name="payment_type" class="form-control form-control-sm" id="payment_type" required/>
-                          <option>Cash</option>
-                          <option>Card</option>
-                          <option>Draft</option>
-                          <option>Cheque</option>
-                        </select>
-                      </div>
-                    </div>
-
-                    <center>
-                      <input type="submit" id="order_form" style="width:150px;" class="btn btn-info" value="Order">
-                      <input type="submit" id="print_invoice" style="width:150px;" class="btn btn-success d-none" value="Print Invoice">
-                    </center>
-
-
-				  	</form>
-
-				  </div>
-				</div>
-			</div>
-		</div>				</div>
-				<!-- /Page Content -->
-				
-			</div>
-			<!-- /Page Wrapper -->
-			
-        </div>
-		<!-- /Main Wrapper -->
-	<!-- jQuery -->
-	<script src="assets/js/jquery-3.5.1.min.js?v=<?php echo time();?>""></script>
-	<script src="assets/js/popper.min.js?v=<?php echo time();?>""></script>
-	<script src="assets/js/bootstrap.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/jquery.slimscroll.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/jquery.dataTables.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/dataTables.bootstrap4.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/select2.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/app.js?v=<?php echo time();?>"></script>	
-	<script src="script.js?v=<?php echo time();?>"></script>
-</section>
-
-
-</body>
-</html>
-<?php 
-}else{
-     header("Location: index.php");
-     exit();
-}
- ?> >
+            </div>
+            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+               <h3>À,</h3>
+               <div class="form-group">
+                  <input type="text" class="form-control" name="companyName" id="companyName" placeholder="Company Name" autocomplete="off">
+               </div>
+               <div class="form-group">
+                  <textarea class="form-control" rows="3" name="address" id="address" placeholder="Your Address"></textarea>
+               </div>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+               <table class="table table-condensed table-striped" id="invoiceItem">
+                  <tr>
+                     <th width="2%">
+                      <div class="custom-control custom-checkbox mb-3">
+                        <input type="checkbox" class="custom-control-input" id="checkAll" name="checkAll">
+                        <label class="custom-control-label" for="checkAll"></label>
+                        </div>
+                    </th>
+                     <th width="15%">Item No</th>
+                     <th width="38%">Item Name</th>
+                     <th width="15%">Quantity</th>
+                     <th width="15%">Price</th>
+                     <th width="15%">Total</th>
+                  </tr>
+                  <tr>
+                     <td><div class="custom-control custom-checkbox">
+                        <input type="checkbox" class="itemRow custom-control-input" id="itemRow_1">
+                        <label class="custom-control-label" for="itemRow_1"></label>
+                        </div></td>
+                     <td><input type="text" name="productCode[]" id="productCode_1" class="form-control" autocomplete="off"></td>
+                     <td><input type="text" name="productName[]" id="productName_1" class="form-control" autocomplete="off"></td>
+                     <td><input type="number" name="quantity[]" id="quantity_1" class="form-control quantity" autocomplete="off"></td>
+                     <td><input type="number" name="price[]" id="price_1" class="form-control price" autocomplete="off"></td>
+                     <td><input type="number" name="total[]" id="total_1" class="form-control total" autocomplete="off"></td>
+                  </tr>
+               </table>
+            </div>
+         </div>
+         <div class="row">
+            <div class="col-xs-12">
+               <button class="btn btn-danger delete" id="removeRows" type="button">- Delete</button>
+               <button class="btn btn-success" id="addRows" type="button">+ Add More</button>
+            </div>
+         </div>
+         <div class="row">
+          <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="form-group mt-3 mb-3 ">
+              <label>Subtotal: &nbsp;</label>
+                 <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text currency">$</span>
+            </div>
+            <input value="" type="number" class="form-control" name="subTotal" id="subTotal" placeholder="Subtotal">
+          </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="form-group mt-3 mb-3 ">
+              <label>Tax Rate: &nbsp;</label>
+                 <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text currency">%</span>
+            </div>
+           <input value="" type="number" class="form-control" name="taxRate" id="taxRate" placeholder="Tax Rate">
+          </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="form-group mt-3 mb-3 ">
+              <label>Tax Amount: &nbsp;</label>
+                 <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text currency">$</span>
+            </div>
+            <input value="" type="number" class="form-control" name="taxAmount" id="taxAmount" placeholder="Tax Amount">
+          </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="form-group mt-3 mb-3 ">
+              <label>Total: &nbsp;</label>
+                 <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text currency">$</span>
+            </div>
+             <input value="" type="number" class="form-control" name="totalAftertax" id="totalAftertax" placeholder="Total">
+          </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="form-group mt-3 mb-3 ">
+              <label>Amount Paid: &nbsp;</label>
+                 <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text currency">$</span>
+            </div>
+            <input value="" type="number" class="form-control" name="amountPaid" id="amountPaid" placeholder="Amount Paid">
+          </div>
+              </div>
+          </div>
+          <div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+            <div class="form-group mt-3 mb-3 ">
+              <label>Amount Due: &nbsp;</label>
+                 <div class="input-group mb-3">
+            <div class="input-group-prepend">
+              <span class="input-group-text currency">$</span>
+            </div>
+             <input value="" type="number" class="form-control" name="amountDue" id="amountDue" placeholder="Amount Due">
+          </div>
+              </div>
+          </div>
+            <div class="col-xs-12 col-sm-8 col-md-8 col-lg-8">
+               <h3>Notes: </h3>
+               <div class="form-group">
+                  <textarea class="form-control txt" rows="5" name="notes" id="notes" placeholder="Your Notes"></textarea>
+               </div>
+               <br>
+               <div class="form-group">
+                  <input type="hidden" value="<?php echo $_SESSION['userid']; ?>" class="form-control" name="userId">
+                  <input data-loading-text="Saving Invoice..." type="submit" name="invoice_btn" value="Save Invoice" class="btn btn-success submit_btn invoice-save-btm">           
+               </div>
+            </div>
+         </div>
+         <div class="clearfix"></div>
+      </div>
+   </form>
+     </div>
+   </div>
+</div>
+</div>	
