c8b98490f80a9496bc6d5e22033f721574b2d881
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-23 14:12:12 +0100

    Update sol.sql

--- sol.sql
+++ sol.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : lun. 23 août 2021 à 10:49
+-- Généré le : lun. 23 août 2021 à 15:11
 -- Version du serveur : 10.4.20-MariaDB
 -- Version de PHP : 8.0.8
 
@@ -48,7 +48,7 @@ CREATE TABLE `clients` (
 --
 
 INSERT INTO `clients` (`id_client`, `rs_client`, `ref_client`, `fj_client`, `email_client`, `adresse_client`, `ville_client`, `pays_client`, `tel_client`, `sw_client`, `mf_client`, `riprib_client`, `tauxtva_client`) VALUES
-(2, 'Houssem', 'CA1920', 'SA', 'houssem.hannachi@enis.tn', 'Rue el Afrah', 'Metouia', 'Tunisie', '222222', 'www.houssem.Com', '13267', 12222, 212112);
+(8, 'Houssem', 'CA1920', 'SA', 'houssem.hannachi@enis.tn', 'Rue el Afrah', 'Metouia', 'Tunisie', '52215947', 'www.houssem.Com', 'qdsdsq', 12222, 212112);
 
 -- --------------------------------------------------------
 
@@ -58,14 +58,52 @@ INSERT INTO `clients` (`id_client`, `rs_client`, `ref_client`, `fj_client`, `ema
 
 CREATE TABLE `devis` (
   `nom_client` text NOT NULL,
-  `id_client` int(11) NOT NULL,
+  `id_devis` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `adresse_client` text NOT NULL,
-  `reference_client` text NOT NULL,
-  `date` date NOT NULL,
-  `designation` text NOT NULL,
-  `quantite` int(11) NOT NULL,
-  `TVA` int(11) NOT NULL
+  `date` datetime NOT NULL DEFAULT current_timestamp(),
+  `baseht` double NOT NULL,
+  `remise` double NOT NULL,
+  `totalht` double NOT NULL,
+  `TVArate` int(11) NOT NULL,
+  `TVAamount` int(11) NOT NULL,
+  `totalttc` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `devis`
+--
+
+INSERT INTO `devis` (`nom_client`, `id_devis`, `user_id`, `adresse_client`, `date`, `baseht`, `remise`, `totalht`, `TVArate`, `TVAamount`, `totalttc`) VALUES
+('MAMAAAM', 8, 1, 'MAMMAMMA', '2021-08-23 14:05:16', 1, 1, 50, 2, 1, 51);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `devis_item`
+--
+
+CREATE TABLE `devis_item` (
+  `id_devis` int(11) NOT NULL,
+  `item_code` varchar(255) NOT NULL,
+  `item_name` varchar(255) NOT NULL,
+  `order_item_quantity` int(11) NOT NULL,
+  `order_item_price` int(11) NOT NULL,
+  `order_item_final_amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `devis_item`
+--
+
+INSERT INTO `devis_item` (`id_devis`, `item_code`, `item_name`, `order_item_quantity`, `order_item_price`, `order_item_final_amount`) VALUES
+(5, '55', '0', 10, 10, 100),
+(6, '1', '0', 3, 300, 900),
+(7, 'REFJHSJ', 'SitewEB', 1, 10000, 10000),
+(7, '322', 'ORdinateur', 2, 9877, 19754),
+(8, '2112', 'MAMAMAMA', 5, 10, 50),
+(9, 'ZZAZ', 'ZAZAZA', 4444, 4, 17776),
+(9, '2212', 'SQSQ', 33, 33, 1089);
 
 -- --------------------------------------------------------
 
@@ -76,6 +114,7 @@ CREATE TABLE `devis` (
 CREATE TABLE `invoice_order` (
   `user_id` int(11) NOT NULL,
   `order_id` int(11) NOT NULL,
+  `order_receiver_id` int(11) NOT NULL,
   `order_receiver_name` varchar(255) NOT NULL,
   `order_receiver_address` varchar(255) NOT NULL,
   `order_total_before_tax` varchar(255) NOT NULL,
@@ -88,18 +127,6 @@ CREATE TABLE `invoice_order` (
   `note` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 ROW_FORMAT=COMPACT;
 
---
--- Déchargement des données de la table `invoice_order`
---
-
-INSERT INTO `invoice_order` (`user_id`, `order_id`, `order_receiver_name`, `order_receiver_address`, `order_total_before_tax`, `order_total_tax`, `order_tax_per`, `order_total_after_tax`, `order_amount_paid`, `order_total_amount_due`, `order_date`, `note`) VALUES
-(1, 5, 'Assestit Company', '247 Clan Alpine Way', '4116', '288.12', '7', '4404.12', '4404.12', '0', '2021-07-12 00:06:31', 'none at the moment!!'),
-(1, 13, 'HAHHAHAH', 'HAHHAHHAHA', '49377284', '987545.68', '2', '50364829.68', '50364829', '0.6799999997019768', '2021-08-20 15:38:27', ''),
-(1, 14, 'Ghassen', 'Metouia', '2000', '100', '5', '2100', '100', '2000', '2021-08-20 15:48:07', ''),
-(1, 16, 'Houssem', 'meya metouia', '2950', '59', '2', '3009', '0', '3009', '2021-08-21 10:43:55', ''),
-(1, 17, 'Houssem', 'Metouia', '25000', '1250', '5', '26250', '250', '26000', '2021-08-21 14:40:44', ''),
-(1, 18, 'Ahmed', 'Gabès', '550', '104.5', '19', '654.5', '54', '600.5', '2021-08-23 09:11:32', '');
-
 -- --------------------------------------------------------
 
 --
@@ -107,8 +134,8 @@ INSERT INTO `invoice_order` (`user_id`, `order_id`, `order_receiver_name`, `orde
 --
 
 CREATE TABLE `invoice_order_item` (
-  `order_id` int(11) NOT NULL,
   `item_code` varchar(255) NOT NULL,
+  `order_id` int(11) DEFAULT NULL,
   `item_name` varchar(255) NOT NULL,
   `order_item_quantity` varchar(255) NOT NULL,
   `order_item_price` varchar(255) NOT NULL,
@@ -119,22 +146,8 @@ CREATE TABLE `invoice_order_item` (
 -- Déchargement des données de la table `invoice_order_item`
 --
 
-INSERT INTO `invoice_order_item` (`order_id`, `item_code`, `item_name`, `order_item_quantity`, `order_item_price`, `order_item_final_amount`) VALUES
-(8, '2', 'PC', '50', '200', '10000'),
-(9, '1', 'Book', '100', '100', '10000'),
-(10, 'ZZAE', 'sqddsqd', '2', '2', '4'),
-(11, 'sss', 'ssss', '3', '5', '15'),
-(12, 'SSSsss', 'sSSSSSSSSSSssss', '4', '11', '44'),
-(13, '33', '22', '2222', '22222', '49377284'),
-(15, '22', 'GHJNK?L', '5', '5', '25'),
-(15, '2345', 'fghjklm', '10', '4', '40'),
-(15, '', '', '', '', '0'),
-(14, '2', 'SiteWeb', '10', '200', '2000'),
-(16, '1', 'PC Gamer', '1', '2700', '2700'),
-(16, '3', 'Clavier', '5', '50', '250'),
-(17, '1', 'Ordinateur', '5', '1000', '5000'),
-(17, '2', 'Camera', '1', '20000', '20000'),
-(18, '1', 'Ordinateur', '55', '10', '550');
+INSERT INTO `invoice_order_item` (`item_code`, `order_id`, `item_name`, `order_item_quantity`, `order_item_price`, `order_item_final_amount`) VALUES
+('1', 21, 'SiteWeb', '1', '10', '10');
 
 -- --------------------------------------------------------
 
@@ -175,14 +188,6 @@ CREATE TABLE `paiements` (
   `prix` double NOT NULL,
   `status_paiement` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Déchargement des données de la table `paiements`
---
-
-INSERT INTO `paiements` (`id_paiement`, `rs_client`, `id_client`, `date_paiement`, `mode_de_paiement`, `id_facture`, `prix`, `status_paiement`) VALUES
-(2, 'Houssem', 2, '2021-08-01', 'Chèque', 4, 6, 'Payé'),
-(3, 'Houssem', 2, '2021-08-15', 'Espèces', 4, 10, 'Sent');
 
 -- --------------------------------------------------------
 
@@ -241,10 +246,17 @@ ALTER TABLE `clients`
   ADD PRIMARY KEY (`id_client`) USING BTREE;
 
 --
+-- Index pour la table `devis`
+--
+ALTER TABLE `devis`
+  ADD PRIMARY KEY (`id_devis`);
+
+--
 -- Index pour la table `invoice_order`
 --
 ALTER TABLE `invoice_order`
-  ADD PRIMARY KEY (`order_id`);
+  ADD PRIMARY KEY (`order_id`),
+  ADD KEY `id_client` (`order_receiver_id`);
 
 --
 -- Index pour la table `invoice_user`
@@ -257,7 +269,8 @@ ALTER TABLE `invoice_user`
 --
 ALTER TABLE `paiements`
   ADD PRIMARY KEY (`id_paiement`),
-  ADD KEY `test2` (`id_client`);
+  ADD KEY `test2` (`id_client`),
+  ADD KEY `test3` (`id_facture`);
 
 --
 -- Index pour la table `propects`
@@ -279,13 +292,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT pour la table `clients`
 --
 ALTER TABLE `clients`
-  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_client` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT pour la table `devis`
+--
+ALTER TABLE `devis`
+  MODIFY `id_devis` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT pour la table `invoice_order`
 --
 ALTER TABLE `invoice_order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT pour la table `invoice_user`
@@ -316,10 +335,17 @@ ALTER TABLE `users`
 --
 
 --
+-- Contraintes pour la table `invoice_order`
+--
+ALTER TABLE `invoice_order`
+  ADD CONSTRAINT `order_receiver_id` FOREIGN KEY (`order_receiver_id`) REFERENCES `clients` (`id_client`);
+
+--
 -- Contraintes pour la table `paiements`
 --
 ALTER TABLE `paiements`
-  ADD CONSTRAINT `test2` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id_client`);
+  ADD CONSTRAINT `test2` FOREIGN KEY (`id_client`) REFERENCES `clients` (`id_client`),
+  ADD CONSTRAINT `test3` FOREIGN KEY (`id_facture`) REFERENCES `invoice_order` (`order_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
