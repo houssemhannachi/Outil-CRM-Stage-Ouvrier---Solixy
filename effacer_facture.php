4656a078f11e5c27327a61ac40fec1d6aa7de277
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-24 12:45:45 +0100

    Update effacer_facture.php

--- effacer_facture.php
+++ effacer_facture.php
@@ -7,7 +7,7 @@ include 'db_conn.php';
 if (isset($_GET['order_id']))
 {
     $order_id=$_GET['order_id'];
-    $deleteQuery="DELETE FROM invoice_order where order_id=$order_id"; 
+    $deleteQuery="DELETE FROM facture where id_facture=$order_id"; 
     mysqli_query($conn, $deleteQuery);
 
     echo "<script>window.location = 'facture.php';</script>";
