4363d0d225d9c6be24faaccbf27c0b6b376eb788
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-27 13:15:31 +0100

    Update clients.php

--- clients.php
+++ clients.php
@@ -389,18 +389,7 @@ if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 	<!-- /Main Wrapper -->
 	
 	<!-- jQuery -->
-	<script src="assets/js/bootstrap.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/jquery.slimscroll.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/jquery.dataTables.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/dataTables.bootstrap4.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/select2.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/app.js?v=<?php echo time();?>"></script>	
-	<script src="assets/js/script.js?v=<?php echo time();?>"></script>
-</section>
-
-
-</body>
-</html>
+<?php include('footer.php')?>
 <?php 
 }else{
      header("Location: index.php");
