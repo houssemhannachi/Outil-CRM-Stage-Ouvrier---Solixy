87ae54325df53200073c05257be4daa89e17b582
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-27 12:41:25 +0100

    Update facture.php

--- facture.php
+++ facture.php
@@ -109,7 +109,7 @@ $invoice = new Invoice();
 <script src="assets/js/jquery.dataTables.min.js?v=<?php echo time();?>"></script>
 <script src="assets/js/dataTables.bootstrap4.min.js?v=<?php echo time();?>"></script>
 <script src="assets/js/app.js?v=<?php echo time();?>"></script>	
-<script src="script.js?v=<?php echo time();?>"></script>	
+<script src="assets/js/script.js?v=<?php echo time();?>"></script>	
 </section>
 
 
