ddeaed7f7ecff2033e7aeee8f87e93f78eccd3d4
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-27 17:18:52 +0100

    Update effacer_paiement.php

--- effacer_paiement.php
+++ effacer_paiement.php
@@ -1,7 +1,7 @@
 <?php 
     require_once("db_conn.php");
-    if (isset($_POST['deletedata'])){
-            $id = $_POST['delete_id'];
+    if (isset($_GET['id_paiement'])){
+            $id = $_GET['id_paiement'];
             $query = "DELETE FROM paiements WHERE id_paiement='$id'";
             $result = mysqli_query($conn,$query);
     
