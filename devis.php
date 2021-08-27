78dbc658e675d3631b759adf37dea28b32196c24
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-27 13:15:33 +0100

    Update devis.php

--- devis.php
+++ devis.php
@@ -99,12 +99,4 @@ $dev = new Dev();
         ?>
       </table>	
 </div>	
-<script src="assets/js/jquery.dataTables.min.js?v=<?php echo time();?>"></script>
-<script src="assets/js/dataTables.bootstrap4.min.js?v=<?php echo time();?>"></script>
-<script src="assets/js/app.js?v=<?php echo time();?>"></script>	
-<script src="assets/js/script.js?v=<?php echo time();?>"></script>	
-</section>
-
-
-</body>
-</html>
+<?php include('footer.php')?>
