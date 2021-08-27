c2040501d4a8adf7b254b3fbc7521cca94893c5b
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-27 13:15:36 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,13 @@
+
+</section>
+</body> 
+    <script src="assets/js/bootstrap.min.js?v=<?php echo time();?>"></script>
+	<script src="assets/js/jquery.slimscroll.min.js?v=<?php echo time();?>"></script>
+	<script src="assets/js/jquery.dataTables.min.js?v=<?php echo time();?>"></script>
+	<script src="assets/js/dataTables.bootstrap4.min.js?v=<?php echo time();?>"></script>
+	<script src="assets/js/select2.min.js?v=<?php echo time();?>"></script>
+	<script src="assets/js/app.js?v=<?php echo time();?>"></script>	
+	<script src="assets/js/script.js?v=<?php echo time();?>"></script>
+
+
+</html>
