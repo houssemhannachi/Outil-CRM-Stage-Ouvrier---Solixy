1474fc9094adc89503f095c311042879684b338e
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-27 13:15:38 +0100

    Update home.php

--- home.php
+++ home.php
@@ -75,22 +75,8 @@ session_start();
       </div>
 
 
+      <?php include('footer.php')?>
 
-
-</section>
-</body> 
-<script src="assets/js/bootstrap.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/jquery.slimscroll.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/jquery.dataTables.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/dataTables.bootstrap4.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/select2.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/app.js?v=<?php echo time();?>"></script>	
-	<script src="assets/js/script.js?v=<?php echo time();?>"></script>
-
-
-
-
-</html>
 <?php 
 }else{
      header("Location: index.php");
