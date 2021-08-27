7814474e209c424c21324abc0c0848d393b0b325
houssemhannachi <72234639+houssemhannachi@users.noreply.github.com>
2021-08-27 13:56:07 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <title> <?php echo $pageName ?> </title>
     <link rel="stylesheet" href="assets/css/styledash.css?v=<?php echo time();?>" >
-    <link rel="stylesheet" href="css/style.css?v=<?php echo time();?>" >
+    <link rel="stylesheet" href="assets/css/styleindex.css?v=<?php echo time();?>">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css?v=<?php echo time();?>">
 		<link rel="stylesheet" href="assets/css/font-awesome.min.css?v=<?php echo time();?>">
     <link rel="stylesheet" href="assets/css/line-awesome.min.css?v=<?php echo time();?>">
@@ -13,6 +13,8 @@
     <link rel="stylesheet" href="assets/css/styleinvoice.css?v=<?php echo time();?>">
         
     <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
