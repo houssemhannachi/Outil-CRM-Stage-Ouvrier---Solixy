334f201ccae9ddf1b123a111524f5851644e0719
houssemhannachi <houssem.hannachi@enis.tn>
2021-10-10 15:50:01 +0100

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,10 @@
-
 </section>
-</body> 
-    <script src="assets/js/bootstrap.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/jquery.slimscroll.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/jquery.dataTables.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/dataTables.bootstrap4.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/select2.min.js?v=<?php echo time();?>"></script>
-	<script src="assets/js/app.js?v=<?php echo time();?>"></script>	
-	<script src="assets/js/script.js?v=<?php echo time();?>"></script>
-
-
+</body>
+<script src="assets/js/bootstrap.min.js?v=<?php echo time(); ?>"></script>
+<script src="assets/js/jquery.slimscroll.min.js?v=<?php echo time(); ?>"></script>
+<script src="assets/js/jquery.dataTables.min.js?v=<?php echo time(); ?>"></script>
+<script src="assets/js/dataTables.bootstrap4.min.js?v=<?php echo time(); ?>"></script>
+<script src="assets/js/select2.min.js?v=<?php echo time(); ?>"></script>
+<script src="assets/js/app.js?v=<?php echo time(); ?>"></script>
+<script src="assets/js/script.js?v=<?php echo time(); ?>"></script>
 </html>
