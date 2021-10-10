2331a203cc731f3c55e9a76beb00c4e769bf6a5a
houssemhannachi <houssem.hannachi@enis.tn>
2021-10-10 15:49:54 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -6,40 +6,52 @@ require("header.php")
     <img id="logo_name" src="assets\images\logo1.png">
     <img id="solixy">
   </div>
-<ul class="nav-links">
-    <li class = "<?php if($pageName == 'Home') {echo 'act';}?>">
+  <ul class="nav-links">
+    <li class="<?php if ($pageName == 'Home') {
+                  echo 'act';
+                } ?>">
       <a href="home.php">
-        <i class='bx bx-home-alt' ></i>
+        <i class='bx bx-home-alt'></i>
         <span class="links_name">Accueil</span>
       </a>
     </li>
-    <li class = "<?php if($pageName == 'Clients') {echo 'act';}?>" >
+    <li class="<?php if ($pageName == 'Clients') {
+                  echo 'act';
+                } ?>">
       <a href="clients.php">
-        <i class='bx bx-user' ></i>
-        <span class ="links_name"> Clients</span>
+        <i class='bx bx-user'></i>
+        <span class="links_name"> Clients</span>
       </a>
     </li>
-    <li class = "<?php if($pageName == 'Prospects') {echo 'act';}?>">
+    <li class="<?php if ($pageName == 'Prospects') {
+                  echo 'act';
+                } ?>">
       <a href="prospects.php">
-        <i class='bx bx-loader-circle' ></i>
+        <i class='bx bx-loader-circle'></i>
         <span class="links_name">Prospects</span>
-      </a> 
+      </a>
     </li>
-    <li class = "<?php if($pageName == 'Factures') {echo 'act';}?>">
+    <li class="<?php if ($pageName == 'Factures') {
+                  echo 'act';
+                } ?>">
       <a href="facture.php">
-        <i class='bx bx-copy-alt' ></i>
+        <i class='bx bx-copy-alt'></i>
         <span class="links_name">Factures</span>
       </a>
     </li>
-    <li class = "<?php if($pageName == 'Devis') {echo 'act';}?>">
+    <li class="<?php if ($pageName == 'Devis') {
+                  echo 'act';
+                } ?>">
       <a href="devis.php">
-        <i class='bx bx-dollar-circle' ></i>
+        <i class='bx bx-dollar-circle'></i>
         <span class="links_name">Devis</span>
       </a>
     </li>
-    <li class = "<?php if($pageName == 'Paiements') {echo 'act';}?>">
+    <li class="<?php if ($pageName == 'Paiements') {
+                  echo 'act';
+                } ?>">
       <a href="paiments.php">
-        <i class='bx bx-dollar' ></i>
+        <i class='bx bx-dollar'></i>
         <span class="links_name">Paiements</span>
       </a>
     </li>
@@ -52,50 +64,51 @@ require("header.php")
   </ul>
 </div>
 <section class="home-section">
-    <nav>
-      <div class="sidebar-button">
-        <i class='bx bx-menu sidebarBtn'></i>
-        <span class="dashboard">Dashboard</span>
-      </div>
-      <div class="search-box">
-        <form action="" method="POST">
-          <input type="text" placeholder="Recherche..." name="recherche">
-          <button class="bx bx-search" type="submit" name="recherche-button"></button>
-        </form>
-      </div>
-      <div class="profile-details">
-        <img src="assets/images/profill.jpg" alt="">
-        <span class="admin_name"><?php echo $_SESSION['name']; ?></span>
-        <div class="dropdown">
-          <button class="bx bx-chevron-down" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a href="logout.php"><i class='bx bx-log-out'></i><span class="log">Déconnexion</span> </a>
-          </div>
+  <nav>
+    <div class="sidebar-button">
+      <i class='bx bx-menu sidebarBtn'></i>
+      <span class="dashboard">Dashboard</span>
+    </div>
+    <div class="search-box">
+      <form action="" method="POST">
+        <input type="text" placeholder="Recherche..." name="recherche">
+        <button class="bx bx-search" type="submit" name="recherche-button"></button>
+      </form>
+    </div>
+    <div class="profile-details">
+      <img src="assets/images/profill.jpg" alt="">
+      <span class="admin_name"><?php echo $_SESSION['name']; ?></span>
+      <div class="dropdown">
+        <button class="bx bx-chevron-down" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></button>
+        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+          <a href="logout.php"><i class='bx bx-log-out'></i><span class="log">Déconnexion</span> </a>
         </div>
       </div>
-    </nav>
+    </div>
+  </nav>
 
-<?php
+  <?php
 
-if (isset($_POST['recherche-button'])){
-  $recherche = mb_strtoupper($_POST['recherche']);
+  if (isset($_POST['recherche-button'])) {
+    $recherche = mb_strtoupper($_POST['recherche']);
 
-  switch ($recherche) {
+    switch ($recherche) {
       case "CLIENTS":
-          header ('location:clients.php');
-          break;
+        header('location:clients.php');
+        break;
       case "FACTURES":
-          header ('location:facture.php');
-          break;
+        header('location:facture.php');
+        break;
       case "PROSPECTS":
-           header ('location:prospects.php');
-          break;
+        header('location:prospects.php');
+        break;
       case "DEVIS":
-            header ('location:devis.php');
-           break;
+        header('location:devis.php');
+        break;
       case "PAIEMENTS":
-            header ('location:paiments.php');
-           break;
-  }}
+        header('location:paiments.php');
+        break;
+    }
+  }
 
-?>
+  ?>
