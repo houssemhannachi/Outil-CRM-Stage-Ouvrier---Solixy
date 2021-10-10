9a8c0aab3ba78d50558e9783a9da566b60ede29c
houssemhannachi <houssem.hannachi@enis.tn>
2021-10-10 15:49:55 +0100

    Update db_conn.php

--- db_conn.php
+++ db_conn.php
@@ -1,13 +1,8 @@
 <?php
 
-$sname= "localhost";
-$unmae= "root";
-$password = "";
+	$sname= "localhost";
+	$unmae= "root";
+	$password = "";
+	$db_name = "sol";
 
-$db_name = "sol";
-
-$conn = mysqli_connect($sname, $unmae, $password, $db_name);
-
-if (!$conn) {
-	die('Please check your connection'.mysqli_error());
-} 
+	$conn = mysqli_connect($sname, $unmae, $password, $db_name);
