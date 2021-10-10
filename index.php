d8a43d96203449b65a343b781760c732f3ba09e0
houssemhannachi <houssem.hannachi@enis.tn>
2021-10-10 15:50:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,27 +2,22 @@
 <html lang="en">
 
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
-
   <title>CRM SOLIXY</title>
   <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,400,500,600,700,800,900" rel="stylesheet">
   <link href="https://fonts.googleapis.com/css?family=Lato:100,100i,300,300i,400,400i,700,700i,900,900i" rel="stylesheet">
-
-  <link href="assets/css/styleindex.css?v=<?php echo time();?>" rel="stylesheet">
-  <link rel = "icon" href = "assets/images/icons.png" type = "image/x-icon">   
-
+  <link href="assets/css/styleindex.css?v=<?php echo time(); ?>" rel="stylesheet">
+  <link rel="icon" href="assets/images/icons.png" type="image/x-icon">
 </head>
 
 <body>
 
-  <!-- Navigation -->
   <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
     <div class="container">
-      <a href="http://www.solixy.com/" ><img id="logo" src="assets/images/logo.png"> </a>
+      <a href="http://www.solixy.com/"><img id="logo" src="assets/images/logo.png"> </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
@@ -31,7 +26,7 @@
           <li class="nav-item">
             <a class="nav-link" href="">Admin</a>
           </li>
-            <li class="nav-item">
+          <li class="nav-item">
             <a class="nav-link" href="http://www.solixy.com/contact">Contact</a>
           </li>
         </ul>
@@ -47,35 +42,32 @@
       </div>
     </div>
     <div class="login-page">
-        <div class="form">
+      <div class="form">
         <form action="login.php" method="post">
-     	
-     	<input type="text" name="uname" placeholder="Nom d'utilisateur"><br>
-     	<input type="password" name="password" placeholder="Mot de passe"><br>
 
-     	<button type="submit">Connexion</button>
-       <?php if (isset($_GET['error'])) { ?>
-     		<p class="error"><?php echo $_GET['error']; ?></p>
-     	<?php } ?>
-     </form>
-        </div>
+          <input type="text" name="uname" placeholder="Nom d'utilisateur"><br>
+          <input type="password" name="password" placeholder="Mot de passe"><br>
+
+          <button type="submit">Connexion</button>
+          <?php if (isset($_GET['error'])) { ?>
+            <p class="error"><?php echo $_GET['error']; ?></p>
+          <?php } ?>
+        </form>
       </div>
+    </div>
     <div class="bg-circle-1 bg-circle"></div>
     <div class="bg-circle-2 bg-circle"></div>
     <div class="bg-circle-3 bg-circle"></div>
     <div class="bg-circle-4 bg-circle"></div>
-  
-  
-</header>
-  
+  </header>
 
 
-  <!-- Footer -->
+
+
   <footer class="py-5 bg-black">
     <div class="container">
       <p class="m-0 text-center text-white small">SOLIXY 2021</p>
     </div>
-    <!-- /.container -->
   </footer>
 
 </body>
